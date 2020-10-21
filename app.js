da1d999b7635deadd0e770170946e78c5861fc34
Shivranjan Kumar <shivranjan77@gmail.com>
2020-10-22 00:13:59 +0530

    final

---
+++ app.js
@@ -0,0 +1,69 @@
+var express=require("express");
+var mongoose=require("mongoose");
+var bodyParser=require("body-parser");
+var app=express();
+
+app.set('view engine','ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser:true,useUnifiedTopology:true});
+const itemSchema={
+    name:String
+}
+const Item=mongoose.model("Item",itemSchema);
+const item1=new Item({
+    name:"Welcome to Shivranjan App",
+});
+
+const wel=[item1];
+
+app.get("/",function(req,res)
+{
+   Item.find({},function(err,f)
+   {
+      if(f.length===0)
+      {
+        Item.insertMany(wel,function(err)
+        {
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("Successfully saved items to DB");
+            }
+        });
+      res.redirect("/");
+      }
+      else{
+      res.render("list",{newListItems:f});
+      }
+   })
+  ;
+})
+app.post("/",function(req,res)
+{
+     const itemName=req.body.n;
+     const item=new Item({
+       name:itemName
+   });
+item.save();
+res.redirect("/");
+});
+app.post("/delete",function(req,res)
+{
+  const check=req.body.checkbox;
+  Item.findByIdAndRemove(check,function(err)
+  {
+      if(!err)
+      {
+          console.log("Successfully deleted");
+          res.redirect("/");
+      }
+  })
+});
+
+app.listen(4000,function()
+{
+    console.log("Server is listening to port 4000");
+})
+
